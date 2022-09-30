95458c91dd622e13c6b6396c1553d73db974dec3
Parth Patel <parth8733@gmail.com>
2022-09-29 22:53:27 -0400

    Implemented Pokémon Bag container

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View style={styles.bagContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            alert("Displaying Data from Database");
+          }}
+        >
+          <View style={styles.bagButton}>
+            <Text style={styles.bagButtonText}>Pokémons Bag</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={{ flex: 4 }}></View>
     </View>
-  )
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bagContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  bagButton: {
+    padding: 10,
+    backgroundColor: "lightblue",
+    borderRadius: "40",
+    marginRight: 20,
+  },
+  bagButtonText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
