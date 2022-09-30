2ffe6b5cbe815adf796a01930ead252f8629a01b
Parth Patel <parth8733@gmail.com>
2022-09-29 23:45:57 -0400

    implemented pokemon list

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+import React, { useEffect } from "react";
+import usePokemonsApi from "../hooks/usePokemonsApi";
+import PokemonItem from "./PokemonItem";
 
 export default function Home() {
+  const [{ data, loading, error }, searchPokemons] = usePokemonsApi();
+  useEffect(() => {
+    searchPokemons();
+  }, []);
+  if (loading) {
+    return <ActivityIndicator size={"large"} marginVertical={150} />;
+  }
+  if (error) {
+    return <Text style={styles.header}>{error}</Text>;
+  }
   return (
     <View style={styles.container}>
       <View style={styles.bagContainer}>
@@ -15,7 +34,16 @@ export default function Home() {
           </View>
         </TouchableOpacity>
       </View>
-      <View style={{ flex: 4 }}></View>
+      <View style={styles.pokemonListContainer}>
+        <FlatList
+          data={data}
+          keyExtractor={(pokemon) => pokemon.name}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => (
+           <PokemonItem pokemon={item} />
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -34,9 +62,15 @@ const styles = StyleSheet.create({
     borderRadius: "40",
     marginRight: 20,
   },
+  header: {
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
+  },
   bagButtonText: {
     fontWeight: "bold",
     fontSize: 20,
     textAlign: "center",
   },
+  pokemonListContainer: { flex: 4, marginHorizontal: 25 }
 });
